d4039be58482cc4e11632cfb9b34efcb706afd1e
Piotr Kosturek <pkosturek123@gmail.com>
2020-03-05 16:03:06 +0100

    configured Firebase, added basic activity to add users

--- app/src/main/java/farszownicy/caldirola/MainActivity.kt
+++ app/src/main/java/farszownicy/caldirola/MainActivity.kt
@@ -2,29 +2,80 @@ package farszownicy.caldirola
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val NAME_KEY = "name"
+        const val SURN_KEY = "surname"
+        const val TAG = "debug"
+    }
+    private val db = FirebaseFirestore.getInstance()
+    private val userDoc = db.collection("users").document("RL68xbDoaO34qCnG1pti")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addButton.setOnClickListener {
+            addUser()
+        }
+        fetch_button.setOnClickListener{
+            fetchUserData()
+        }
 
-        // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+        /**
+         * Nasłuchiwanie zmian w  danym dokumencie.
+         * activity = this oznacza, że nasłuchiwanie nie będzie działało jak Activity się zatrzyma
+         */
+        userDoc.addSnapshotListener(this
+        ) { snapshot, e ->
+            if (e != null) {
+                Log.w(TAG, "Listen failed.", e)
+                return@addSnapshotListener
+            }
+
+            if (snapshot != null && snapshot.exists()) {
+                Log.d(TAG, "Current data: ${snapshot.data}")
+            } else {
+                Log.d(TAG, "Current data: null")
+            }
+        }
     }
 
     /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
+     * Pobiera dane userów z serwera
      */
-    external fun stringFromJNI(): String
-
-    companion object {
-
-        // Used to load the 'native-lib' library on application startup.
-        init {
-            System.loadLibrary("native-lib")
+    private fun fetchUserData() {
+        db.collection("users").get().addOnSuccessListener { result -> for (document in result) {
+            val user = document.toObject(User::class.java)
+            Log.d(TAG, user.toString())
+            //Log.d(TAG, "${document.id} : ${document.data}")
         }
+        }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+    }
+
+    /**
+     * Dodaje usera do Firestore'a
+     */
+    private fun addUser(){
+        val userName = nameET.text.toString()
+        val userSurname = surnameET.text.toString()
+
+        if(userName.isEmpty() || userSurname.isEmpty())
+            return
+        val userData = hashMapOf(
+            NAME_KEY to userName,
+            SURN_KEY to userSurname
+        )
+
+        db.collection("users").add(userData).addOnSuccessListener {
+                documentReference -> Log.d(TAG, "user added with ID: ${documentReference.id}")  }
+            .addOnFailureListener{e -> Log.w(TAG, "Error adding user", e)}
     }
 }
---
+++ app/src/main/java/farszownicy/caldirola/User.kt
@@ -0,0 +1,4 @@
+package farszownicy.caldirola
+
+data class User(var id: String = "-1", var name: String = "", var surname: String = "") {
+}
