f0a90c6dc22a6669badb83cb266811a74b91c197
Piotr Kosturek <pkosturek123@gmail.com>
2020-03-14 00:39:31 +0100

    messy code, but app can now display default calendar view by Tibolte

---
+++ app/src/main/java/farszownicy/caldirola/activities/DrawableCalendarEvent.kt
@@ -0,0 +1,50 @@
+package farszownicy.caldirola.activities
+import com.github.tibolte.agendacalendarview.models.BaseCalendarEvent
+import com.github.tibolte.agendacalendarview.models.CalendarEvent
+import java.util.*
+
+
+class DrawableCalendarEvent : BaseCalendarEvent {
+    // endregion
+// region Public methods
+    var drawableId: Int
+
+    // region Constructors
+    constructor(
+        id: Long,
+        color: Int,
+        title: String?,
+        description: String?,
+        location: String?,
+        dateStart: Long,
+        dateEnd: Long,
+        allDay: Int,
+        duration: String?,
+        drawableId: Int
+    ) : super(id, color, title, description, location, dateStart, dateEnd, allDay, duration) {
+        this.drawableId = drawableId
+    }
+
+    constructor(
+        title: String?,
+        description: String?,
+        location: String?,
+        color: Int,
+        startTime: Calendar?,
+        endTime: Calendar?,
+        allDay: Boolean,
+        drawableId: Int
+    ) : super(title, description, location, color, startTime, endTime, allDay) {
+        this.drawableId = drawableId
+    }
+
+    constructor(calendarEvent: DrawableCalendarEvent) : super(calendarEvent) {
+        drawableId = calendarEvent.drawableId
+    }
+
+    // endregion
+// region Class - BaseCalendarEvent
+    override fun copy(): CalendarEvent {
+        return DrawableCalendarEvent(this)
+    } // endregion
+}
---
+++ app/src/main/java/farszownicy/caldirola/model/Event.kt
@@ -0,0 +1,69 @@
+package farszownicy.caldirola.model
+
+import com.github.tibolte.agendacalendarview.models.CalendarEvent
+import com.github.tibolte.agendacalendarview.models.DayItem
+import com.github.tibolte.agendacalendarview.models.WeekItem
+import java.util.*
+
+class Event : CalendarEvent{
+    override fun setId(mId: Long) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setDayReference(mDayReference: DayItem?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getId(): Long {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setInstanceDay(mInstanceDay: Calendar?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setWeekReference(mWeekReference: WeekItem?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun copy(): CalendarEvent {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getDayReference(): DayItem {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getWeekReference(): WeekItem {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setStartTime(mStartTime: Calendar?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setEndTime(mEndTime: Calendar?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setTitle(mTitle: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getTitle(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getEndTime(): Calendar {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getStartTime(): Calendar {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getInstanceDay(): Calendar {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
