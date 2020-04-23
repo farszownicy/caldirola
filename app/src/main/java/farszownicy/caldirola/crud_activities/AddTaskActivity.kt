b0d2a370ab878c8a214fc40e7677ebd57e56865d
Patrykm107 <patrykm987@gmail.com>
2020-04-23 02:38:16 +0200

    Fixed spinner

--- app/src/main/java/farszownicy/caldirola/crud_activities/AddTaskActivity.kt
+++ app/src/main/java/farszownicy/caldirola/crud_activities/AddTaskActivity.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -12,9 +15,11 @@ import com.google.firebase.firestore.FirebaseFirestore
 import farszownicy.caldirola.Logic.PlanManager
 import farszownicy.caldirola.R
 import farszownicy.caldirola.activities.MainActivity
+import farszownicy.caldirola.data_classes.Place
 import farszownicy.caldirola.data_classes.Task
 import farszownicy.caldirola.utils.Constants
 import farszownicy.caldirola.utils.DateTimeUtils
+import kotlinx.android.synthetic.main.activity_add_event.*
 import kotlinx.android.synthetic.main.activity_add_task.*
 import java.lang.Exception
 import java.time.LocalTime
@@ -22,7 +27,8 @@ import java.util.*
 import kotlin.time.ExperimentalTime
 import kotlin.time.minutes
 
-class AddTaskActivity : AppCompatActivity() {
+class AddTaskActivity : AppCompatActivity(),
+    AdapterView.OnItemSelectedListener{
     companion object
     {
         const val NAME_KEY = "name"
@@ -52,17 +58,7 @@ class AddTaskActivity : AppCompatActivity() {
             addTask()
         }
 
-        locations.get()
-            .addOnSuccessListener { documents ->
-                for(document in documents){
-                    Log.d(AddEventActivity.TAG, "${document.id} => ${document.data}")
-                    val name = document.getString(NAME_KEY)
-                    places.add(name!!)
-                }
-            }.addOnFailureListener { exception ->
-                Log.w(TAG, "Error getting documents: ", exception)
-            }
-
+        setSpinner()
         setAllDatePickers()
 
         userDoc.addSnapshotListener(this
@@ -83,7 +79,22 @@ class AddTaskActivity : AppCompatActivity() {
             }
         }
     }
-
+    private fun setSpinner() {
+        locations.get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                    val name = document.getString(NAME_KEY)
+                    places.add(name!!)
+                }
+                val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, places)
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                at_location.onItemSelectedListener = this@AddTaskActivity
+                at_location.adapter = adapter
+            }.addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents: ", exception)
+            }
+    }
     private fun setAllDatePickers()
     {
         calendarUtils.setDatePicker(at_deadline_date, this@AddTaskActivity)
@@ -100,7 +111,7 @@ class AddTaskActivity : AppCompatActivity() {
         val name = at_input_name.text.toString()
         val description = at_input_description.text.toString()
         val deadline = calendarUtils.getDTFromTV(at_deadline_date, at_deadline_time)
-        //val selected_location = places
+        val selected_location = Place(at_location.selectedItem as String)
         val divisible = at_divisible.isChecked
         val priority = at_input_priority.text.toString()
         val duration: Int
@@ -120,13 +131,14 @@ class AddTaskActivity : AppCompatActivity() {
             NAME_KEY to name,
             DESCRIPTION_KEY to description,
             DEADLINE_KEY to deadline,
-            //LOCATION_KEY to selected_location,
+            LOCATION_KEY to selected_location,
             DIVISIBILITY_KEY to divisible,
             PRIORITY_KEY to priority,
             DURATION_KEY to duration
         )
         val task = Task(UUID.randomUUID().toString(), name, description,
-            deadline, duration.minutes, priority= priority.toInt(),divisible=divisible)
+            deadline, duration.minutes, priority= priority.toInt(),divisible=divisible, places = listOf(selected_location)
+        )
         val taskIntent = Intent()
 
         val taskAdded = PlanManager.addTask(task)
@@ -134,7 +146,6 @@ class AddTaskActivity : AppCompatActivity() {
             db.collection("tasks").add(task_data).addOnSuccessListener { documentReference ->
                 Log.d(TAG, "Task added with ID: ${documentReference.id}")
             }.addOnFailureListener { e -> Log.w(TAG, "Error adding task", e) }
-            //TODO saveTaskToInternalMemory
             taskIntent.putExtra(Constants.ADD_TASK_KEY, taskAdded)
             setResult(Activity.RESULT_OK, taskIntent)
             finish()
@@ -144,5 +155,11 @@ class AddTaskActivity : AppCompatActivity() {
         }
     }
 
+    //SPINNER METHODS
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+    }
 }
